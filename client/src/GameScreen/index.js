4503138599cb7eae4d5a7f258b4214727c30b612
nkalia1802 <n.kalia@student.tudelft.nl>
2021-05-12 19:25:19 +0200

    Add skeleton game screen

---
+++ client/src/GameScreen/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container'
+
+export default function GameScreen() {
+    return (
+        <Container fluid>
+            <h1>Research Project: GWAP</h1>
+        </Container>
+    )
+}
