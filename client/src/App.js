2308ad94e7e401542558d84a18ec6349839c2c74
nkalia1802 <n.kalia@student.tudelft.nl>
2021-05-12 23:23:39 +0200

    socketio connection works

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,20 @@
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import Routes from "./Routes";
-
+import socketClient  from "socket.io-client";
 
 function App() {
+    const [message, setMessage] = useState('begin');
+    const SERVER = "http://127.0.0.1:8000";
+    let socket = socketClient (SERVER);
+    socket.on('message', (m) => {
+        setMessage(m)});
+
     return (
+        <>
             <Routes/>
+            <h1>{message}</h1>
+        </>
     );
 }
 
