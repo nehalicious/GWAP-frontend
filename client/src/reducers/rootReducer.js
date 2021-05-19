4dfcc2235560ed13b2234ce7c86eb75e020cf9b2
nkalia1802 <n.kalia@student.tudelft.nl>
2021-05-19 22:57:13 +0200

    add round to reducers

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -2,10 +2,15 @@ import {combineReducers} from 'redux';
 import { player, score, player_type } from './playerReducer';
 import { room } from './roomReducer';
 import {session_id, session_scene} from "./sessionReducer";
-import {hint} from './hintReducer'
+import {hint} from './hintReducer';
+import {round} from './roundReducer'
 
 const rootReducer = combineReducers({
-    player, score, player_type, room, session_id, session_scene, hint
+    player, score, player_type,
+    room,
+    session_id, session_scene,
+    round,
+    hint
 });
 
 export default rootReducer;
