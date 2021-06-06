de8d5c2ffc3797fbeb8b5f8b45564adfb20f8806
nkalia1802 <n.kalia@student.tudelft.nl>
2021-06-06 19:21:36 +0200

    fix connections

--- client/src/utils/Receiver.js
+++ client/src/utils/Receiver.js
@@ -13,20 +13,23 @@ export default function Receiver() {
     const dispatch = useDispatch();
     const history = useHistory();
 
-    const [player_id, setPlayerID] = useState('');
+    let player = useSelector(store=>store.player);
+
+    const reset = () => {
+        dispatch(setSelectedHint(''));
+        dispatch(setAllHints([]));
+        dispatch(setGuess(''));
+    };
 
     useEffect(()=> {
         socket.on('player', ({player_obj, room}) => {
             console.log(player_obj);
             console.log(room);
-            setPlayerID(player_obj._id);
             dispatch(setPlayer(player_obj._id));
             dispatch(setScore(player_obj.points));
             dispatch(setPlayerType(player_obj.type));
             dispatch(setRoom(room._id));
-            dispatch(setSelectedHint(''));
-            dispatch(setAllHints([]));
-            dispatch(setGuess(''));
+            reset();
             history.push('/GameScreen')
         });
 
@@ -37,13 +40,11 @@ export default function Receiver() {
         socket.on('session', new_session => {
             console.log('new_session');
             dispatch(setSession(new_session));
-            dispatch(setSelectedHint(''));
-            dispatch(setAllHints([]));
-            dispatch(setGuess(''));
+            reset();
         });
 
         socket.on('receive_hint', message => {
-            console.log(message)// history.push('/Vote')
+            console.log(message)
         });
 
         socket.on('hint', hint => {
@@ -52,45 +53,29 @@ export default function Receiver() {
         });
 
         socket.on('selectedHint', hint => {
-            console.log(hint);
             dispatch(setSelectedHint(hint));
         });
 
         socket.on('guess', guess => {
-            console.log(guess);
             dispatch(setGuess(guess));
         });
 
         socket.on('round', data => {
-            console.log(data);
             dispatch(setRound(data.new_round._id));
-            dispatch(setSelectedHint(''));
-            dispatch(setAllHints([]));
-            dispatch(setGuess(''));
+            reset();
         });
 
         socket.on('update_scores', data => {
-            console.log(data);
-            console.log(player_id);
-            dispatch(setFinalScore(data));
-            for(let i = 0; i<data.players.length; i++) {
-                let current = data.players[i];
-                console.log(current._id);
-                if(current._id === player_id) {
-                    console.log(current.points);
-                    dispatch(setScore(current.points))
-                }
-            }
+           dispatch(setFinalScore(data));
         });
 
         socket.on('game_over', data => {
-            console.log(data);
             dispatch(setFinalScore(data));
             history.push('/GameOver')
         });
 
     }, []);
 
-    return null;
+    return <></>;
 
 }
